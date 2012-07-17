534df0f9003398ed2e41365cc84bd3d289e41d2e
Denys P <dector9@gmail.com>
2012-07-17 14:19:51 +0300

    + Host Checker handler (by Jetty)

--- src/HostCheckerHandler.java
+++ src/HostCheckerHandler.java
@@ -12,29 +12,34 @@ import java.io.PrintWriter;
  * @author dector (dector9@gmail.com)
  */
 public class HostCheckerHandler extends AbstractHandler {
-    private static final String PARAM_HOST      = "host";
+    private static final String PARAM_HOST      = "h";
     private static final String PARAM_TIMEOUT   = "t";
 
-    private static final String RESP_OK         = "OK";
-    private static final String RESP_FAILED     = "Failed";
+    private static final String RESP_OK         = "Active";
+    private static final String RESP_FAILED     = "Not Active";
+
+    private static final int DEFAULT_TIMEOUT    = 1000;
 
     public void handle(String s,
                        Request baseRequest,
                        HttpServletRequest req,
                        HttpServletResponse resp)
             throws IOException, ServletException {
-        System.out.println("sss");
+        if (baseRequest.getPathInfo().equals(("/"))) {
+            String host = req.getParameter(PARAM_HOST);
+            String timeOutStr = req.getParameter(PARAM_TIMEOUT);
+            int timeOut = (timeOutStr != null) ? Integer.parseInt(timeOutStr) : DEFAULT_TIMEOUT;
 
-        String host = req.getParameter(PARAM_HOST);
-        int timeOut = Integer.parseInt(req.getParameter(PARAM_TIMEOUT));
+            System.out.printf("Host: %s, timeout: %d%n", host, timeOut);
 
-        resp.setContentType("text/plain");
-        resp.setStatus(HttpServletResponse.SC_OK);
+            resp.setContentType("text/plain");
+            resp.setStatus(HttpServletResponse.SC_OK);
 
-        baseRequest.setHandled(true);
+            baseRequest.setHandled(true);
 
-        PrintWriter out = resp.getWriter();
-        out.println(getResponseString(host, timeOut));
+            PrintWriter out = resp.getWriter();
+            out.println(getResponseString(host, timeOut));
+        }
     }
 
     private String getResponseString(String host, int timeOut) {
